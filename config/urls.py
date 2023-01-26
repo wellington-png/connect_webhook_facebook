144e4065bbb1d01953a4daf77ad327dc2e630efb
gguerran <gustavoguerra.gr@gmail.com>
2023-01-26 15:29:08 -0300

    adm

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("adm/", admin.site.urls),
     path("api/lead/", include("lead.urls")),
 ]
