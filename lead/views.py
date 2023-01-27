d3c7f7e148b82395e7136d8f5ff590920b8749ca
gguerran <gustavoguerra.gr@gmail.com>
2023-01-26 22:43:23 -0300

    teste form

--- lead/views.py
+++ lead/views.py
@@ -28,11 +28,48 @@ class FacebookLeadAds:
         lead_data = lead.get("field_data", None)
 
         for data in lead_data:
-            if data.get("name", None) in ["e-mail", "email"]:
+            if data.get("name", None) in ["e-mail", "email", "E-mail"]:
                 email = data.get("values")[0]
                 return email.strip().lower()
         print('False')
         return False
+
+    def get_lead_full_name(lead_id):
+        try:
+            print("lead_id")
+            lead = Lead(lead_id).api_get()
+        except FacebookRequestError as e:
+            print('false error')
+            return False
+
+        lead_data = lead.get("field_data", None)
+
+        for data in lead_data:
+            if data.get("name", None) in ["Nome completo", "Nome_completo"]:
+                name = data.get("values")[0]
+                return name.strip().lower()
+        print('False')
+        return False
+
+    def get_lead_telefone(lead_id):
+        try:
+            print("lead_id")
+            lead = Lead(lead_id).api_get()
+        except FacebookRequestError as e:
+            print('false tell error')
+            return False
+
+        lead_data = lead.get("field_data", None)
+
+        for data in lead_data:
+            if data.get("name", None) in ["telefone", "Telefone"]:
+                name = data.get("values")[0]
+                return name.strip().lower()
+        print('False')
+        return False
+
+
+
 class FacebookWebhook(APIView):
     def get(self, request):
         verify_token = request.GET.get("hub.verify_token", "")
@@ -51,8 +88,10 @@ class FacebookWebhook(APIView):
                 leadgen_id = change["value"]["leadgen_id"]
                 print(leadgen_id)
 
-                lead_email = FacebookLeadAds().get_lead_email(leadgen_id)
-                if not lead_email:
+                lead_email = FacebookLeadAds().get_lead_email(532486685526415)
+                fullname = FacebookLeadAds().get_lead_full_name(532486685526415)
+                telefone = FacebookLeadAds().get_lead_telefone(532486685526415)
+                if not lead_email and fullname and telefone:
                     return Response({"success": False})
 
         print('success')
